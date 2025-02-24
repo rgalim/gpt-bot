2290e8c1cbdfb5a614965db7e3440f5443befe81
rgalim <swdevnode@gmail.com>
2025-02-24 15:49:04 +0100

    add web client config

---
+++ src/main/java/com/rgalim/gptbot/config/WebClientConfig.java
@@ -0,0 +1,39 @@
+package com.rgalim.gptbot.config;
+
+import io.netty.resolver.DefaultAddressResolverGroup;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.resources.ConnectionProvider;
+
+import java.time.Duration;
+
+@Configuration
+@EnableConfigurationProperties({
+        WebClientProperties.class,
+        TelegramProperties.class
+})
+public class WebClientConfig {
+
+    @Bean
+    public WebClient webClient(WebClientProperties properties) {
+        ConnectionProvider provider = ConnectionProvider
+                .builder("webClientConnectionProvider")
+                // Ensure maxIdleTime is longer than Telegram timeout value
+                .maxIdleTime(Duration.ofSeconds(properties.maxIdleTime()))
+                .maxLifeTime(Duration.ofSeconds(properties.maxLifeTime()))
+                .build();
+
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(
+                        HttpClient.create(provider)
+                                .resolver(DefaultAddressResolverGroup.INSTANCE)))
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+    }
+}
