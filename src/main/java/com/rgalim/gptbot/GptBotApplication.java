1ce6d7199b9506e93a9d9662198565a8ebf22501
rgalim <swdevnode@gmail.com>
2025-02-24 11:17:18 +0100

    initial commit

---
+++ src/main/java/com/rgalim/gptbot/GptBotApplication.java
@@ -0,0 +1,13 @@
+package com.rgalim.gptbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GptBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GptBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rgalim/gptbot/GptBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rgalim.gptbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GptBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
