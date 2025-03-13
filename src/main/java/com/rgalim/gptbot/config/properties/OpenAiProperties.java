8653b5e69e93d33cb8ea5459c1bcffef1d5caa96
rgalim <swdevnode@gmail.com>
2025-03-13 14:16:15 +0100

    add openai client config

---
+++ src/main/java/com/rgalim/gptbot/config/properties/OpenAiProperties.java
@@ -0,0 +1,12 @@
+package com.rgalim.gptbot.config.properties;
+
+import lombok.NonNull;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "openai")
+public record OpenAiProperties(
+        @NonNull String apiKey,
+        @NonNull String organizationId,
+        @NonNull String projectId
+) {
+}
